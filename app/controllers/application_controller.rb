893c818b38a510d0756478fd3e7ae157e0eac71a
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-04 16:41:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require "./config/environment"
 class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
-  
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
@@ -11,21 +11,43 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
-  get '/recipes/new' do
-    erb :new
-  end
+  get '/recipes/new' do #loads new form
+     erb :new
+   end
 
-  get '/recipes' do
-    @recipes = Recipes.all
-    erb :index
-  end
+   get '/recipes' do #loads index page
+     @recipes = Recipe.all
+     erb :index
+   end
 
-  post '/recipes' do
-    @recipe = Recipe.create(params)
-    redirect to '/recipes/#{recipe.id}'
-  end
+   get '/recipes/:id' do  #loads show page
+     @recipe = Recipe.find_by_id(params[:id])
+     erb :show
+   end
 
+   get '/recipes/:id/edit' do #loads edit form
+     @recipe = Recipe.find_by_id(params[:id])
+     erb :edit
+   end
 
+   patch '/recipes/:id' do  #updates a recipe
+     @recipe = Recipe.find_by_id(params[:id])
+     @recipe.name = params[:name]
+     @recipe.ingredients = params[:ingredients]
+     @recipe.cook_time = params[:cook_time]
+     @recipe.save
+     redirect to "/recipes/#{@recipe.id}"
+   end
 
+   post '/recipes' do  #creates a recipe
+     @recipe = Recipe.create(params)
+     redirect to "/recipes/#{@recipe.id}"
+   end
 
-end
+   delete '/recipes/:id' do #destroy action
+     @recipe = Recipe.find_by_id(params[:id])
+     @recipe.delete
+     redirect to '/recipes'
+   end
+
+ end
