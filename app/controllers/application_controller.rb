bf6c9cca1e1a56dd327a79738927e66886834f3b
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-04 16:38:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,21 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipes.all
+    erb :index
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to '/recipes/#{recipe.id}'
+  end
+
+
+
+
 end
